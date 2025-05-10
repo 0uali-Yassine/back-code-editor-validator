95178c6a990ba1d572ab391c79c2c9a171772d7b
0uali-Yassine <yassinzerouali0@gmail.com>
2025-05-10 19:43:10 +0100

    add role to the signup & to token

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -7,7 +7,7 @@ const bcrypt = require('bcryptjs');
 
 const signUp = async (req, res) => {
     try {
-      const { name, email, password } = req.body;
+      const { name, email, password , role } = req.body;
 
       if(!name || !email || !password){
           return res.status(400).json({message:"Please remplair All the fields"});
@@ -17,7 +17,8 @@ const signUp = async (req, res) => {
       if (isExist) return res.status(401).json({ message: 'User Already Exist!' });
 
       const hashed = await bcrypt.hash(password, 10);
-      const user = new User({ name, email, password: hashed });
+
+      const user = new User({ name, email, password: hashed ,role});
       const userData = await user.save();
 
       return res.status(200).json({status : true ,data :  userData});
@@ -30,6 +31,7 @@ const signUp = async (req, res) => {
 
 const Login = async (req, res) => {
   const { email, password } = req.body;
+
   const user = await User.findOne({ email });
   if (!user) return res.status(401).json({ message: 'User not found' });
 
@@ -37,7 +39,7 @@ const Login = async (req, res) => {
   if (!isMatch) return res.status(401).json({ message: 'Invalid credentials' });
 
   const token = jwt.sign(
-    { userId: user._id }, 
+    { userId: user._id , role:user.role }, 
     process.env.JWT_SECRET,
     { expiresIn: '24h' }
 );
